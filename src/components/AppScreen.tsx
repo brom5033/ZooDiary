c0b0e4c38f271e7181b3026e5af7edb969849fb2
root <root@DESKTOP-JBS8DDR>
2023-10-31 00:49:23 +0900

    feat(appscreen): pc화면 전용 웹 ui 구현

--- src/components/AppScreen.tsx
+++ src/components/AppScreen.tsx
@@ -1,12 +1,11 @@
 import type { FC, PropsWithChildren } from 'react';
 import { AppScreen as StackFlowAppScreen } from '@stackflow/plugin-basic-ui';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 // component
 import { AppBar } from '@components/bar/AppBar';
 import { MenuBar } from '@components/bar/MenuBar';
 import { MyPageBar } from '@components/bar/MyPageBar';
 import { ButtonBar } from '@components/bar/ButtonBar';
-
 
 const style = {
     spacing: {
@@ -16,22 +15,30 @@ const style = {
         display: 'flex',
     },
 } as const;
+
 interface Props extends PropsWithChildren {
     main?: boolean;
     sub?: boolean;
     page?: boolean;
-    logout?: boolean
+    logout?: boolean;
 }
 export const AppScreen: FC<Props> = ({ children, main, sub, page, logout }) => {
     return (
         <StackFlowAppScreen>
-            {main && <MenuBar />}
-            {sub && <AppBar />}
-            {page && <MyPageBar />}
-            {logout && <ButtonBar />}
-            <Container component="main" sx={style.spacing} maxWidth={false}>
-                {children}
-            </Container>
+            <Grid container direction="row">
+                <Grid item sx={{ display: { xs: 'none', md: 'block' } }} md={8}>
+                    test
+                </Grid>
+                <Grid item sm={12} md={4}>
+                    {main && <MenuBar />}
+                    {sub && <AppBar />}
+                    {page && <MyPageBar />}
+                    {logout && <ButtonBar />}
+                    <Container component="main" sx={style.spacing} maxWidth={false}>
+                        {children}
+                    </Container>
+                </Grid>
+            </Grid>
         </StackFlowAppScreen>
     );
 };
